656c037690ac5a77660a4eaff10b5b738bd658c7
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2025-03-27 19:26:18 +0800

    Deployment Update UI

--- js/msc.js
+++ js/msc.js
@@ -26,3 +26,52 @@
     }
 
     // getScreenLock();
+
+function hardReload() {
+
+    $.ajax({
+        url: window.location.pathname + "?refresh=" + new Date().getTime(),
+        headers: {
+            "Pragma": "no-cache",
+            "Cache-Control": "no-cache, no-store, must-revalidate",
+            "Expires": "0"
+        }
+    }).done(function () {
+        window.location.reload(true);
+    });
+}
+
+const PROJECT_ID = "prj_zWXe0vOldJ8rnwrhBTxKawnXXBKU";
+const TOKEN = "QtRRZQY0PY6caQhxwUAiXzVy";
+const API_URL = `https://api.vercel.com/v6/deployments?projectId=${PROJECT_ID}`;
+
+let lastDeployment = null;
+
+const updateBtn = document.querySelector("#updateBtn");
+
+async function checkNewDeployment() {
+    try {
+        const response = await fetch(API_URL, {
+            headers: { Authorization: `Bearer ${TOKEN}` }
+        });
+        const data = await response.json();
+        if (data.deployments && data.deployments.length > 0) {
+            const latestDeployment = data.deployments[0];
+            if (!lastDeployment) {
+                lastDeployment = latestDeployment.uid;
+                return;
+            }
+            if (latestDeployment.uid !== lastDeployment) {
+                console.log("New deployment detected!");
+                // window.location.reload(); // Reload to get the latest version
+                updateBtn.style.display = "block";
+            }
+        }
+    } catch (error) {
+        console.error("Error checking for new deployment:", error);
+    }
+}
+
+// Poll every 30 seconds
+setInterval(checkNewDeployment, 30000);
+
